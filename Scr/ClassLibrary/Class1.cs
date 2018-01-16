02266980622beecb8152deffeb5440ee1d737825
jaekthesnaek <32323708+jaekthesnaek@users.noreply.github.com>
2018-01-16 14:52:28 +0100

    Lagt till en solution som vi skapade under dagens lektion

---
+++ Scr/ClassLibrary/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Class1
+    {
+
+        public int AddNumbers (int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+    }
+}
---
+++ Scr/UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("381e000d-56af-4aae-be04-aff572549017")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
