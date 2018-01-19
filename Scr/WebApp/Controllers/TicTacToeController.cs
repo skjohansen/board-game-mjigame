a2048c149dd82f5f6e071b0d2fc90e10fd8bf772
jaekthesnaek <32323708+jaekthesnaek@users.noreply.github.com>
2018-01-19 13:42:14 +0100

    hej

--- Scr/WebApp/Controllers/TicTacToeController.cs
+++ Scr/WebApp/Controllers/TicTacToeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using WebApp.Models;
@@ -12,24 +13,19 @@ namespace WebApp.Controllers
         
         public ActionResult Index()
         {
-            Session.Add("gameId", 3);
+            HttpCookie cookie = new HttpCookie("playerID");
+            cookie.Value = "success";
+            Response.SetCookie(cookie);
+
+
             return View();
         }
-
+        //Session.Add("gameId", 3);
         // /Default/StartGame?nickname=Nickckkke
-        public ActionResult StartGame(string nickname)
-        {
-            return View();
-        }
-
-        public ActionResult Page2()
-        {
-            return View(/*Session["username"]*/);
-        }
 
         public ActionResult ClientInformation()
         {
-
+            string testString = Request.Cookies.Get("playerID").Value;
             return View(/*Session["username"]*/);
             // create a view which have an input, <input type="text" name="Nickname" /> and a button eg "Start game"
             // when clicking the button access the controller StartGame with the parameter nickname
@@ -37,5 +33,12 @@ namespace WebApp.Controllers
             // show the nickname on StartGame view
             // on View2 which shows the content of Session["nickname"]
         }
+
+        public ActionResult Game()
+        {
+            return View();
+        }
+
+
     }
 }
