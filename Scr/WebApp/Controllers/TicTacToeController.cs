32af576319df5e4d0035d29c723e668128ecc48b
jaekthesnaek <32323708+jaekthesnaek@users.noreply.github.com>
2018-01-23 11:11:54 +0100

    ändrat massa

--- Scr/WebApp/Controllers/TicTacToeController.cs
+++ Scr/WebApp/Controllers/TicTacToeController.cs
@@ -4,90 +4,22 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using WebApp.Models;
 
 namespace WebApp.Controllers
 {
     public class TicTacToeController : Controller
     {
+        
 
-        //public ActionResult Index()
-        //{
-        //    Session.Add("gameId", 3);
-        //    return View();
-        //}
-
-
-        //////        public ActionResult Index()
-        //////        {
-        //////<<<<<<< HEAD
-        //////            HttpCookie cookie = new HttpCookie("playerID");
-        //////            cookie.Value = "success";
-        //////            Response.SetCookie(cookie);
-
-        //////=======
-        //////            ViewData["Message"] = "Welcome to ASP.NET MVC!";
-        //////            string cookie = "There is no cookie!";
-        //////            if (this.ControllerContext.HttpContext.Request.Cookies.AllKeys.Contains("Cookie"))
-        //////            {
-        //////                cookie = "Yeah - Cookie: " + this.ControllerContext.HttpContext.Request.Cookies["Cookie"].Value;
-        //////            }
-        //////            ViewData["Cookie"] = cookie;
-        //////            return View();
-        //////        }
-
-
-
-
-
-
-        //        // /Default/StartGame?nickname=Nickckkke
-        //        public ActionResult StartGame(string nickname)
-        //        {
-        //            return View();
-        //        }
-        //>>>>>>> 9cd43f28d1be0b1caed8a68d48a9f444bf54a08e
-
-        //            return View();
-        //        }
-        //Session.Add("gameId", 3);
-        // /Default/StartGame?nickname=Nickckkke
-
-        // create a view which have an input, <input type="text" name="Nickname" /> and a button eg "Start game"
-        // when clicking the button access the controller StartGame with the parameter nickname
-        // save the result in a session variable, eg. Session["nickname] = value of input
-        // show the nickname on StartGame view
-        // on View2 which shows the content of Session["nickname"]
-        //string testString = Request.Cookies.Get("playerID").Value;
-
-        //PlayerModel player = new PlayerModel();
-
-
-        public ActionResult Index()
+        public ActionResult Login()
         {
-
-            return View();
-        }
-        public ActionResult ClientInformation()
-        {
-            PlayerModel player = new PlayerModel();
             return View();
         }
 
-        public ActionResult GetUserName(PlayerModel player)
+        public ActionResult Game(string UserName)
         {
-            //HttpCookie aCookie = new HttpCookie("CookieName");
-            //aCookie.Value = DateTime.Now.ToString();
-            //aCookie.Expires = DateTime.Now.AddMinutes(5);
-            //Response.Cookies.Add(aCookie);
-            //player.UserID = aCookie.Value;
-
-
-
-            return View(player);
-
+            return View();
         }
-
 
 
     }
