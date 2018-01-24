908c3ab73b44e7c4c9713336c5d78fe5d0e87582
Michae Alamin <michaelalamin@gmail.com>
2018-01-24 11:07:18 +0100

    Michael  New

--- Scr/WebApp/Controllers/Client.cs
+++ Scr/WebApp/Controllers/Client.cs
@@ -20,6 +20,19 @@ namespace WebApp.Controllers
     }
 
 
+    //public class Client
+    //{
+    //    public string Name { get; set; }
+    //    public Client Opponent { get; set; }
+    //    public bool IsPlaying { get; set; }
+    //    public bool WaitingForMove { get; set; }
+    //    public bool LookingForOpponent { get; set; }
+    //    public DateTime GameStarted { get; set; }
+    //    public string Simbolo { get; set; }
+
+    //    public string ConnectionId { get; set; }
+    //}
+
 
 
 
