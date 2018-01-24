f26adb0e6d040380abf0aab7927f255873739881
Michae Alamin <michaelalamin@gmail.com>
2018-01-24 14:13:53 +0100

    Revert "Revert "Revert "Merge branch 'master' of https://github.com/TeknikhogskolanGothenburg/board-game-mjigame"""
    This reverts commit 58ede09e59d70362519f6453abbbe28140257b35.

---
+++ TicTacToe/TicTacToe/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+
+       
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.A1 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(242, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "file";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // A1
+            // 
+            this.A1.BackColor = System.Drawing.Color.Gold;
+            this.A1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("A1.BackgroundImage")));
+            this.A1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.A1.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.A1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A1.ForeColor = System.Drawing.Color.Cyan;
+            this.A1.Location = new System.Drawing.Point(0, 24);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(80, 80);
+            this.A1.TabIndex = 2;
+            this.A1.UseVisualStyleBackColor = false;
+            this.A1.Click += new System.EventHandler(this.button_click);
+            // 
+            // A2
+            // 
+            this.A2.BackColor = System.Drawing.Color.Gold;
+            this.A2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("A2.BackgroundImage")));
+            this.A2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.A2.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.A2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A2.ForeColor = System.Drawing.Color.Cyan;
+            this.A2.Location = new System.Drawing.Point(81, 24);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(80, 80);
+            this.A2.TabIndex = 3;
+            this.A2.UseVisualStyleBackColor = false;
+            this.A2.Click += new System.EventHandler(this.button_click);
+            // 
+            // B2
+            // 
+            this.B2.BackColor = System.Drawing.Color.Gold;
+            this.B2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("B2.BackgroundImage")));
+            this.B2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.B2.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.B2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B2.ForeColor = System.Drawing.Color.Cyan;
+            this.B2.Location = new System.Drawing.Point(81, 105);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(80, 80);
+            this.B2.TabIndex = 5;
+            this.B2.UseVisualStyleBackColor = false;
+            this.B2.Click += new System.EventHandler(this.button_click);
+            // 
+            // B1
+            // 
+            this.B1.BackColor = System.Drawing.Color.Gold;
+            this.B1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("B1.BackgroundImage")));
+            this.B1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.B1.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.B1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B1.ForeColor = System.Drawing.Color.Cyan;
+            this.B1.Location = new System.Drawing.Point(0, 105);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(80, 80);
+            this.B1.TabIndex = 4;
+            this.B1.UseVisualStyleBackColor = false;
+            this.B1.Click += new System.EventHandler(this.button_click);
+            // 
+            // C2
+            // 
+            this.C2.BackColor = System.Drawing.Color.Gold;
+            this.C2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("C2.BackgroundImage")));
+            this.C2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.C2.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.C2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C2.ForeColor = System.Drawing.Color.Cyan;
+            this.C2.Location = new System.Drawing.Point(81, 186);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(80, 80);
+            this.C2.TabIndex = 7;
+            this.C2.UseVisualStyleBackColor = false;
+            this.C2.Click += new System.EventHandler(this.button_click);
+            // 
+            // C1
+            // 
+            this.C1.BackColor = System.Drawing.Color.Gold;
+            this.C1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("C1.BackgroundImage")));
+            this.C1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.C1.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.C1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C1.ForeColor = System.Drawing.Color.Cyan;
+            this.C1.Location = new System.Drawing.Point(0, 186);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(80, 80);
+            this.C1.TabIndex = 6;
+            this.C1.UseVisualStyleBackColor = false;
+            this.C1.Click += new System.EventHandler(this.button_click);
+            // 
+            // C3
+            // 
+            this.C3.BackColor = System.Drawing.Color.Gold;
+            this.C3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("C3.BackgroundImage")));
+            this.C3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.C3.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.C3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C3.ForeColor = System.Drawing.Color.Cyan;
+            this.C3.Location = new System.Drawing.Point(162, 186);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(80, 80);
+            this.C3.TabIndex = 10;
+            this.C3.UseVisualStyleBackColor = false;
+            this.C3.Click += new System.EventHandler(this.button_click);
+            // 
+            // B3
+            // 
+            this.B3.BackColor = System.Drawing.Color.Gold;
+            this.B3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("B3.BackgroundImage")));
+            this.B3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.B3.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.B3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B3.ForeColor = System.Drawing.Color.Cyan;
+            this.B3.Location = new System.Drawing.Point(162, 105);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(80, 80);
+            this.B3.TabIndex = 9;
+            this.B3.UseVisualStyleBackColor = false;
+            this.B3.Click += new System.EventHandler(this.button_click);
+            // 
+            // A3
+            // 
+            this.A3.BackColor = System.Drawing.Color.Gold;
+            this.A3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("A3.BackgroundImage")));
+            this.A3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.A3.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.A3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A3.ForeColor = System.Drawing.Color.Cyan;
+            this.A3.Location = new System.Drawing.Point(162, 24);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(80, 80);
+            this.A3.TabIndex = 8;
+            this.A3.UseVisualStyleBackColor = false;
+            this.A3.Click += new System.EventHandler(this.button_click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(242, 267);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button A3;
+    }
+}
+
